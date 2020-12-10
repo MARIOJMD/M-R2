0b5776c7898d0a00df4ae1f68fe30ab8ac9c24fd
MMAGANADEBIA <mmaganadebia@localhost.localdomain>
2020-12-09 19:39:10 -0600

    R2

--- App.js
+++ App.js
@@ -1,14 +1,42 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {Component} from 'react';
+import { StyleSheet, Text, View, Image, TextInput, Button} from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      text1: "",
+      text2: ""
+    }
+  }
+  mostrarTexto(){
+    alert(this.state.text2)
+    alert(this.state.text1)
+  }
+  changeText(){
+    console.log("Escribiendo...");
+  }
+  render() {
+    return (
+      <View style={styles.container}>
+        <Image source={require('./assets/287849.jpg')} style={styles.bigBox}/>
+        <TextInput
+          style={styles.mediumBox} 
+          onChangeText={text1=>this.setState({text1: text1})}
+        />
+        <TextInput
+          style={styles.mediumBox}
+          onChangeText={text2=>this.setState({text2: text2})}
+        />
+        <Button
+          title="Mostrar texto"
+          color="#9b9b9b"
+          onPress={()=>this.mostrarTexto()}
+        />
+      </View>
+    );
+ }
 }
 
 const styles = StyleSheet.create({
@@ -16,6 +44,16 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'space-around',
   },
+  bigBox: {
+    width: 300,
+    height: 125,
+ },
+  mediumBox: {
+    width: 300,
+    height: 30,
+    borderColor: "orange",
+    borderWidth: 1
+  }
 });
